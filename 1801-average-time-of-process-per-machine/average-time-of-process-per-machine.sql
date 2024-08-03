a29510ffa833e59e17fe31ab05ddbd59482ee4d5
DeepakRaj.R <166492847+deepakraj28@users.noreply.github.com>
2024-08-03 17:41:29 +0530

    Time: 243 ms (24.18%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT
+a.machine_id,
+round(
+      (SELECT avg(a1.timestamp) FROM Activity a1 WHERE a1.activity_type = 'end' AND a1.machine_id = a.machine_id) - 
+      (SELECT avg(a1.timestamp) FROM Activity a1 WHERE a1.activity_type = 'start' AND a1.machine_id = a.machine_id)
+,3) AS processing_time
+FROM Activity a
+GROUP BY a.machine_id
