29a8b4e9008dde5562d110485205c25d432a6cc3
DeepakRaj.R <166492847+deepakraj28@users.noreply.github.com>
2024-08-19 13:38:55 +0530

    Time: 9 ms (30.36%) | Memory: 42.9 MB (21.91%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.java
@@ -0,0 +1,11 @@
+import java.math.BigInteger;
+class Solution
+{
+    public String multiply(String num1, String num2)
+    {
+      BigInteger b1=new BigInteger(num1);
+      BigInteger b2=new BigInteger(num2);
+      BigInteger res=b1.multiply(b2);
+      return res.toString();
+    }
+}
