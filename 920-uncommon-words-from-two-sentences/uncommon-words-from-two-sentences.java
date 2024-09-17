6cdd8975b69a45ffc796af074632436a08ad708c
DeepakRaj.R <166492847+deepakraj28@users.noreply.github.com>
2024-09-17 12:15:52 +0530

    Time: 2 ms (100.00%) | Memory: 41.7 MB (92.80%) - LeetSync

--- 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.java
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.java
@@ -1,30 +1,27 @@
-class Solution {
-    public String[] uncommonFromSentences(String s1, String s2) {
-        String[] a1 = s1.split(" ");
-        String[] a2 = s2.split(" ");
-        String[] newArr = new String[a1.length + a2.length];
-        ArrayList<String> result = new ArrayList<>();
-        HashMap<String, Integer> map = new HashMap<>();
-        int j = 0;
-        for (int i = 0; i < a1.length; i++) {
-            newArr[i] = a1[i];
+class Solution 
+{
+    public String[] uncommonFromSentences(String s1, String s2) 
+    {
+       String str1[]=s1.split(" ");
+       String str2[]=s2.split(" ");
+       HashMap<String,Integer> map=new HashMap<>();
+       for(int i=0;i<str1.length;i++)
+       {
+         map.put(str1[i],map.getOrDefault(str1[i],0) +1);
+       }
+       for(int i=0;i<str2.length;i++)
+       {
+         map.put(str2[i],map.getOrDefault(str2[i],0) +1);
+       }
+       ArrayList<String> arr=new ArrayList<>(); 
+       for(String itr: map.keySet())
+       {
+        if(map.get(itr)==1)
+        {
+            arr.add(itr);
         }
-        for (int i = a1.length; i < newArr.length; i++) {
-            newArr[i] = a2[j];
-            j++;
-        }
-        for (int i = 0; i < newArr.length; i++) {
-            if (map.containsKey(newArr[i])) {
-                map.put(newArr[i], map.get(newArr[i]) + 1);
-            } else {
-                map.put(newArr[i], 1);
-            }
-        }
-        for (String key : map.keySet()) {
-            if (map.get(key) == 1) {
-                result.add(key);
-            }
-        }
-        return result.toArray(new String[0]);
+       }
+       String[] res=arr.toArray(new String[arr.size()]);
+       return res;
     }
 }
