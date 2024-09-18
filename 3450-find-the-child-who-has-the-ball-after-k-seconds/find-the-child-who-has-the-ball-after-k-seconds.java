eeaaac766166def59028bf8d483848dc959d5d69
DeepakRaj.R <166492847+deepakraj28@users.noreply.github.com>
2024-09-18 14:29:01 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (33.73%) - LeetSync

--- 3450-find-the-child-who-has-the-ball-after-k-seconds/find-the-child-who-has-the-ball-after-k-seconds.java
+++ 3450-find-the-child-who-has-the-ball-after-k-seconds/find-the-child-who-has-the-ball-after-k-seconds.java
@@ -1,19 +1,20 @@
-class Solution {
-    public int numberOfChild(int n, int k) {
-        // Handle edge case where there is only one child
-        if (n == 1) {
-            return 0; // Only one child, they always have the ball
+class Solution
+{
+    public int numberOfChild(int n, int k)
+    {
+        if (n == 1)
+        {
+            return 0; 
         }
-
-        // Calculate effective seconds within the cycle
-        int cycle = 2 * (n - 1);
-        int effectiveK = k % cycle;
-
-        // Determine the position of the ball after effectiveK seconds
-        if (effectiveK <= n - 1) {
-            return effectiveK; // Moving right
-        } else {
-            return cycle - effectiveK; // Moving left
+        int c= 2*(n-1);
+        int eff=k%c;
+        if (eff<=n-1)
+        {
+            return eff; 
+        }
+        else
+        {
+            return c-eff; 
         }
     }
 }
